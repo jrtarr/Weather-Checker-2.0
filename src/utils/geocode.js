4460bb125b3db4be5d0d5417ef16e10f685269f2
James Tarr <jrt1025@gmail.com>
2019-05-30 17:11:58 -0400

    initial commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,27 @@
+const request = require('request')
+
+const mapBoxKey = process.env.MBKEY
+const ipInfoKey = process.env.IPKEY
+
+function geocode(address,callback,ipInfo){
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapBoxKey}`
+
+    request({url,json:true}, (error,response,body)=>{
+        if (error){
+            callback('Error connecting to location services.', undefined)
+        }else if(response.body.error || body.features.length === 0){
+            callback('We couldn\'t find that location. Try another search!', undefined)
+        }
+        else{
+            let coords = []
+            coords.push(body.features[0].center[0],body.features[0].center[1]) //Push longitude then latitude to the coordinates array
+            callback(undefined,{
+                lat: body.features[0].center[1],
+                long: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = { geocode }
---
+++ src/utils/weather.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const darkSkyKey = '***REMOVED***'
+
+function forecast(lat, long, callback){
+    const url = `https://api.darksky.net/forecast/${darkSkyKey}/${lat},${long}`
+    request({url,json:true},(error,response,body)=>{
+        if(error){
+            callback('Error connecting to weather services.',undefined)
+        }else if(response.body.error){
+            callback('We couldn\'t find weather data for that location. Try another search!',undefined)
+        }else{
+            const data = {
+                summary: body.currently.summary.toLowerCase(),
+                temp: body.currently.temperature,
+                precipProb: body.currently.precipProbability
+            }
+            callback(undefined,data)
+        }
+    })
+}
+
+module.exports = {forecast}
